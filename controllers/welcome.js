ea16244657ecd1a229c0969a095922558c822026
gittss <gits1399@gmail.com>
2020-11-25 15:02:18 +0530

    views update

--- controllers/welcome.js
+++ controllers/welcome.js
@@ -5,4 +5,12 @@ router.get('/',(req,res)=>{
     res.render('welcome',{title:'Welcome'})
 })
 
+router.get('/about',(req,res)=>{
+    res.render('about',{title:'About Us'})
+})
+
+router.get('/contact',(req,res)=>{
+    res.render('contact',{title:'Contact Us'})
+})
+
 module.exports = router
--- models/product.js
+++ models/product.js
@@ -1,8 +1,24 @@
 const mongoose = require('mongoose')
+const User=require('./user')
 
 const productSchema = mongoose.Schema({
-    name:{type:String, required: true},
-
+    name:{
+        type: String, 
+        required: true
+    },
+    dresscode:{
+        type: String,
+        required: true
+    },
+    size:{
+        type: String,
+        enum: ['X-Small','Small','Medium','Large','X-Large'],
+        default: 'Medium'
+    },
+    customerId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: User
+    }
 })
 
 const Product = mongoose.model('Product',productSchema)
